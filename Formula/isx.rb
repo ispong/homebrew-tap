580bdeb95ad88cedcd6101d758007c49af055d35
ispong <ispong@outlook.com>
2026-06-07 19:31:34 +0800

    升级到v1.1.5

--- Formula/isx.rb
+++ Formula/isx.rb
@@ -7,24 +7,24 @@ class Isx < Formula
   on_macos do
     on_arm do
       url "https://github.com/isxcode/isx-cli/releases/download/v1.1.4/isx_darwin_arm64", using: :nounzip
-      sha256 "5f3ce98a5a27b0ef4bf69dd471595c8b4d1cf013f7b627ba4a18104babc03289"
+      sha256 "e1e497ae228d67b12b24e2239ee556cdf8a8e9cdbc3b8ea9eba85d9d558fbd30"
     end
 
     on_intel do
       url "https://github.com/isxcode/isx-cli/releases/download/v1.1.4/isx_darwin_amd64", using: :nounzip
-      sha256 "6e895901c5cf7bb612affb2b4520afe5bf46a2e465680cf557b5c37b002d61ba"
+      sha256 "d74b3c9d80dfee1d76d676438f327af573d78ac3a7f06f994e101d824475b413"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/isxcode/isx-cli/releases/download/v1.1.4/isx_linux_arm64", using: :nounzip
-      sha256 "44436b8848eabac54e79f30ab5a45556df41719c418ecabcc1e5e2df58513f34"
+      sha256 "eeec1284e376824f7c732b35f11821705fab5467d5263f611d88b086352430fe"
     end
 
     on_intel do
       url "https://github.com/isxcode/isx-cli/releases/download/v1.1.4/isx_linux_amd64", using: :nounzip
-      sha256 "87a60d81270f207e481a16f842b382842d698c0eb828d20ff127bd79a07153fd"
+      sha256 "0ac43b1c8f954cafad66ccf248681ba6dc0a3f2cc89b0c142dc24c1582e0860f"
     end
   end
 
